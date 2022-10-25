fd25e08349b37de5f93c259acf543e72677d79bf
AytacMensimova22 <aytacmensimova22@gmail.com>
2022-10-25 16:56:22 +0400

    js_animation

---
+++ js/index.js
@@ -0,0 +1,17 @@
+ function	col (){
+    // document.getElementById("box").style.transform= "rotateY (360 deg)"
+ 	document.getElementById("box").style.backgroundImage="img/car.png"
+
+ }
+
+ function	img (){
+     	document.getElementById("box").style.backgroundImage="img/car2.png"
+
+ }
+  function olcu(){
+  	document.getElementById("box").style.width="400px"
+  }
+
+  function anim(){
+  	document.getElementById("box").style.animationName="go"
+  }
